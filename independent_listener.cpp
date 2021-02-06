d5ed887ed8064a18d53efc88b569b50afd9fee97
matt <matthewk.rydalch@gmail.com>
2021-02-06 16:54:38 -0700

    independent_listener can be called from python.

--- independent_listener.cpp
+++ independent_listener.cpp
@@ -20,9 +20,47 @@
 #include <gazebo/gazebo_client.hh>
 
 #include <iostream>
+#include <pybind11/pybind11.h>
 
-/////////////////////////////////////////////////
-// Function is called every time a message is received.
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(ilist, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: cmake_example
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
+
+// /////////////////////////////////////////////////
+// // Function is called every time a message is received.
 void posesStampedCallback(ConstPosesStampedPtr &posesStamped)
 {
   std::cout << posesStamped->DebugString();
