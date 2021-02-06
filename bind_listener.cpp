43cd8c46181c0b56ca7d8fdddc223587cd7cd691
matt <matthewk.rydalch@gmail.com>
2021-02-06 16:23:20 -0700

    Cleaned up, and got it building with cmake.

--- bind_listener.cpp
+++ bind_listener.cpp
@@ -9,29 +9,24 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
+PYBIND11_MODULE(bind_listener, m) {
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
-
         .. currentmodule:: cmake_example
-
         .. autosummary::
            :toctree: _generate
-
            add
            subtract
     )pbdoc";
 
     m.def("add", &add, R"pbdoc(
         Add two numbers
-
         Some other explanation about the add function.
     )pbdoc");
 
     m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
         Subtract two numbers
-
         Some other explanation about the subtract function.
     )pbdoc");
 
