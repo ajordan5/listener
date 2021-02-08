6d96014c1e0c3227572f0ded85c15e0465570bc5
matt <matthewk.rydalch@gmail.com>
2021-02-08 12:48:16 -0700

    Boat update function works with the actual boat location.

--- listener.cpp
+++ listener.cpp
@@ -46,32 +46,17 @@ Listener::Listener(int _argc, char **_argv)
 
 void Listener::posesStampedCallback(ConstPosesStampedPtr &posesStamped)
 {
-  boat_.attr("update")(1.0,3.4,-2.3);
-  // auto boat = py::module::import("boat");
-  // boat.attr("Boat");
-  // boat.attr("print_it")();
-  // std::cout << posesStamped->DebugString();
+  for (int i =0; i < posesStamped->pose_size(); ++i)
+  {
+    const ::gazebo::msgs::Pose &pose = posesStamped->pose(i);
+    std::string name = pose.name();
+    if (name == std::string("box"))
+    {
+      const ::gazebo::msgs::Vector3d &position = pose.position();
 
-  // ::google::protobuf::int32 sec = posesStamped->time().sec();
-  // ::google::protobuf::int32 nsec = posesStamped->time().nsec();
-  // std::cout << "Read time: sec: " << sec << " nsec: " << nsec << std::endl;
-
-  // for (int i =0; i < posesStamped->pose_size(); ++i)
-  // {
-  //   const ::gazebo::msgs::Pose &pose = posesStamped->pose(i);
-  //   std::string name = pose.name();
-  //   if (name == std::string("box"))
-  //   {
-  //     const ::gazebo::msgs::Vector3d &position = pose.position();
-
-  //     double x = position.x();
-  //     double y = position.y();
-  //     double z = position.z();
-
-  //     std::cout << "Read position: x: " << x
-  //         << " y: " << y << " z: " << z << std::endl;
-  //   }
-  // }
+      boat_.attr("update")(position.x(),position.y(),position.z());
+    }
+  }
 }
 
 
