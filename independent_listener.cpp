72dde18e35bc6a5b8caa732027578144e3d10c27
matt <matthewk.rydalch@gmail.com>
2021-02-08 09:33:38 -0700

    Able to listen to gazebo c++  and call MAVSDK python.

--- independent_listener.cpp
+++ independent_listener.cpp
@@ -32,74 +32,71 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(ilist, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-        .. currentmodule:: cmake_example
-        .. autosummary::
-           :toctree: _generate
-           add
-           subtract
-    )pbdoc";
+// PYBIND11_MODULE(ilist, m) {
+//     m.doc() = R"pbdoc(
+//         Pybind11 example plugin
+//         -----------------------
+//         .. currentmodule:: cmake_example
+//         .. autosummary::
+//            :toctree: _generate
+//            add
+//            subtract
+//     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
-    )pbdoc");
+//     m.def("add", &add, R"pbdoc(
+//         Add two numbers
+//         Some other explanation about the add function.
+//     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
-    )pbdoc");
+//     m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+//         Subtract two numbers
+//         Some other explanation about the subtract function.
+//     )pbdoc");
 
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
-}
+// #ifdef VERSION_INFO
+//     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+// #else
+//     m.attr("__version__") = "dev";
+// #endif
+// }
 
 // /////////////////////////////////////////////////
 // // Function is called every time a message is received.
 void posesStampedCallback(ConstPosesStampedPtr &posesStamped)
 {
-  std::cout << posesStamped->DebugString();
+  auto platform = py::module::import("platform");
+  platform.attr("print_it")();
+  // std::cout << posesStamped->DebugString();
 
-  ::google::protobuf::int32 sec = posesStamped->time().sec();
-  ::google::protobuf::int32 nsec = posesStamped->time().nsec();
-  std::cout << "Read time: sec: " << sec << " nsec: " << nsec << std::endl;
+  // ::google::protobuf::int32 sec = posesStamped->time().sec();
+  // ::google::protobuf::int32 nsec = posesStamped->time().nsec();
+  // std::cout << "Read time: sec: " << sec << " nsec: " << nsec << std::endl;
 
-  for (int i =0; i < posesStamped->pose_size(); ++i)
-  {
-    const ::gazebo::msgs::Pose &pose = posesStamped->pose(i);
-    std::string name = pose.name();
-    if (name == std::string("box"))
-    {
-      const ::gazebo::msgs::Vector3d &position = pose.position();
+  // for (int i =0; i < posesStamped->pose_size(); ++i)
+  // {
+  //   const ::gazebo::msgs::Pose &pose = posesStamped->pose(i);
+  //   std::string name = pose.name();
+  //   if (name == std::string("box"))
+  //   {
+  //     const ::gazebo::msgs::Vector3d &position = pose.position();
 
-      double x = position.x();
-      double y = position.y();
-      double z = position.z();
+  //     double x = position.x();
+  //     double y = position.y();
+  //     double z = position.z();
 
-      std::cout << "Read position: x: " << x
-          << " y: " << y << " z: " << z << std::endl;
-    }
-  }
+  //     std::cout << "Read position: x: " << x
+  //         << " y: " << y << " z: " << z << std::endl;
+  //   }
+  // }
 }
 
 /////////////////////////////////////////////////
 int main(int _argc, char **_argv)
 {
-  std::cout << "in main. \n";
-
   py::scoped_interpreter python;
 
   py::module_ sys = py::module_::import("sys");
-  sys.attr("path").attr("insert")(1,"../");
-
-  auto matt = py::module::import("matt");
-  matt.attr("print_it")();
+  sys.attr("path").attr("insert")(1,"../../MAVSDK-Python/scripts");
 
   // Load gazebo
   gazebo::client::setup(_argc, _argv);
